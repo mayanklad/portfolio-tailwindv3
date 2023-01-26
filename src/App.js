c57e6f57b0c37337cf751871ca7d0f82c19920f6
mayanklad <mayanklad12@gmail.com>
2023-01-25 22:10:40 -0500

    Added base config for dark light or system theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
 
 function App() {
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  
   return (
     <div className="App">
       Hello World!
     </div>
   );
 }
+
 
 export default App;
