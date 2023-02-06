2de5cf675b766760bf7a024a2804a8a21267c5e2
mayanklad <mayanklad12@gmail.com>
2023-02-06 16:16:33 -0500

    Created base experience layout for sm screen

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,7 @@ import placeholderImage from 'assets/images/placeholder-about-me-image.png';
 
 function About() {  
   return (
-    <div className="about-container px-2.5 py-6 bg-blue-200">
+    <div className="about-container px-2.5 py-6 w-full bg-blue-200">
       <h1 className='px-3.5'>About Me</h1>
       <div className='content px-3.5 w-full flex flex-col items-center'>
         <p className='w-full'>Computer science graduate looking to obtain the position of software developer. I want to work with an organization that can give me ample opportunities to grow so that i will be able to help in the growth of that organization and will be able to enhance my skills and my capabilities.</p>
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,9 +1,49 @@
 import './experience.css';
+import 'react-vertical-timeline-component/style.min.css';
 
-function Experience() {    
+import { ReactComponent as WorkIcon } from 'assets/images/work.svg';
+import { ReactComponent as SchoolIcon } from 'assets/images/school.svg';
+
+import timelineElements from './timelineElements';
+
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+
+function Experience() {  
+  let workIconStyles = {background: '#06D6A0'};
+  let schoolIconStyles = {background: '#f9c74f'};
+  
   return (
-    <div className="Experience">
-      Experience Section
+    <div className="experience-container px-2.5 py-6 w-full bg-green-200">
+      <h1 className='px-3.5'>Experience</h1>
+      <div className='content px-3.5 w-full flex flex-col items-center'>
+        <p className='w-full'>Computer science graduate looking to obtain the position of software developer. I want to work with an organization that can give me ample opportunities to grow so that i will be able to help in the growth of that organization and will be able to enhance my skills and my capabilities.</p>
+        
+        <VerticalTimeline>
+          {timelineElements.map((element) => {
+            let isWorkIcon = element.icon === "work";
+
+            return (
+              <VerticalTimelineElement
+                key={element.key}
+                date={element.date}
+                dateClassName="date"
+                iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <h5 className="vertical-timeline-element-subtitle">
+                  {element.location}
+                </h5>
+                <p id="description">{element.description}</p>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+
+
+      </div>
     </div>
   );
 }
