905891a1e29ae9e997103dc989a441e22327e415
mayanklad <mayanklad12@gmail.com>
2023-02-10 16:39:25 -0500

    Created base interest layout for sm screen

--- src/components/interest/Interest.js
+++ src/components/interest/Interest.js
@@ -1,9 +1,25 @@
 import './interest.css';
 
 function Interest() {  
+  let interests = [
+    'NLP',
+    'Machine Learning',
+    'Collaboration',
+    'Microservice'
+  ];
+
   return (
-    <div className="Interest">
-      Interest Section
+    <div className="interest-container px-2.5 py-6 w-full">
+      <h1 className='px-3.5'>Areas of Interest</h1>
+      <div className='content px-3.5 w-full flex flex-col items-center'>
+        <div className='flex flex-col items-center'>
+          {interests.map((interest, index) => {
+            return (
+              <div key={index}>{interest}</div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
