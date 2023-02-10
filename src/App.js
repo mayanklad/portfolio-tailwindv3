2157b059fac15d02a156f1e56c70c76b77967eb6
mayanklad <mayanklad12@gmail.com>
2023-02-10 16:36:43 -0500

    Enabled tailwind typography

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
   
   return (
-    <div className="App h-screen overflow-y-auto">
+    <div className="App h-screen overflow-y-auto prose">
       <Navbar />
       <Home />
       <About />
