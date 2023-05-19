a07a00c6e970c1289d73dc14e5ac36e61ca87b7e
mayanklad <mayanklad12@gmail.com>
2023-05-19 16:56:54 -0400

    Created base contact layout for sm screen

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,32 @@
 import './contact.css';
+import emailIcon from 'assets/images/email-icon.svg'
+import linkedInIcon from 'assets/images/linkedin-icon.svg'
+import githubIcon from 'assets/images/github-icon.svg'
+import instagramIcon from 'assets/images/instagram-icon.svg'
 
 function Contact() {  
   return (
-    <div className="Contact">
-      Contact Section
+    <div className='contact-container px-2.5 py-6 w-full'>
+      <h1 className='px-3.5'>Contact</h1>
+      <div className='content px-3.5 w-full flex flex-col items-center'>
+        <p className='w-full'>
+          I would love to hear from you! Whether you have a project idea, a question, or just want to chat, feel free to reach out.
+        </p>
+        <div className='flex flex-col items-center'>
+          <a title='Email' className='flex items-center h-14 w-14 mb-3.5' href='mailto:mayanklad12@gmail.com' target='_blank'>
+            <img className='m-0' src={emailIcon} alt='Email' />
+          </a>
+          <a title='LinkedIn' className='flex items-center h-14 w-14 mb-3.5' href='https://www.linkedin.com/in/mayank-lad-602568151/' target='_blank'>
+            <img className='m-0' src={linkedInIcon} alt='LinkedIn' />
+          </a>
+          <a title='Github' className='flex items-center h-14 w-14 mb-3.5' href='https://github.com/mayanklad' target='_blank'>
+            <img className='m-0' src={githubIcon} alt='Github' />
+          </a>
+          <a title='Instagram' className=' flex items-center h-14 w-14 mb-3.5' href='https://www.instagram.com/mayank__lad/' target='_blank'>
+            <img className='m-0' src={instagramIcon} alt='Instagram' />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
