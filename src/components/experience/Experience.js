1007e282bf89373671c26a9f2963abc2ccf24b80
mayanklad <mayanklad12@gmail.com>
2023-09-22 17:15:50 -0400

    Changed timeline order, changed timeline color and experience page bg color

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -9,17 +9,17 @@ import timelineElements from './timelineElements';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 
 function Experience() {  
-  let workIconStyles = {background: '#06D6A0'};
-  let schoolIconStyles = {background: '#f9c74f'};
+  let workIconStyles = {background: '#FFFFFF'};
+  let schoolIconStyles = {background: '#FFFFFF'};
   
   return (
-    <div id='experience' className='experience-container px-2.5 py-6 w-full bg-green-200'>
+    <div id='experience' className='experience-container px-2.5 py-6 w-full bg-[#c4d6e2]'>
       <h1 className='px-3.5'>Experience</h1>
       <div className='content px-3.5 w-full flex flex-col items-center'>
         <p className='w-full'>Computer science graduate looking to obtain the position of software developer. I want to work with an organization that can give me ample opportunities to grow so that i will be able to help in the growth of that organization and will be able to enhance my skills and my capabilities.</p>
         
         <VerticalTimeline>
-          {timelineElements.map((element, index) => {
+          {timelineElements.slice().reverse().map((element, index) => {
             let isWorkIcon = element.icon === "work";
 
             return (
