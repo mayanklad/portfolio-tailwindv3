ac9308e6722d06c377d649af25b07be13bcaa4d2
mayanklad <mayanklad12@gmail.com>
2023-09-23 00:05:19 -0400

    Created init portfolio data file

---
+++ src/components/portfolio/portfolioData.js
@@ -0,0 +1,23 @@
+import googleCaseIcon from 'assets/images/google-case-study.png';
+import compareHouseIcon from 'assets/images/compare-house-desc-charac.png';
+import vehicleCounting from 'assets/images/vehicle-counting.png';
+
+let portfolioData = [
+  {
+    name: 'Google Play Store Case Study',
+    imgUrl: googleCaseIcon,
+    url: 'https://github.com/mayanklad/google-play-store-apps-case-study',
+  },
+  {
+    name: 'Comparison: House Description vs Listed Characteristics',
+    imgUrl: compareHouseIcon,
+    url: 'https://github.com/mayanklad/house-description-verification',
+  },
+  {
+    name: 'Vehicle Counting',
+    imgUrl: vehicleCounting,
+    url: 'https://github.com/mayanklad/vehicle-counting',
+  },
+];
+
+export default portfolioData;
