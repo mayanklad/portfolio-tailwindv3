acf1ee6b29935b9c5779719d077787fab6ee1d1a
mayanklad <mayanklad12@gmail.com>
2023-09-23 00:07:44 -0400

    Replaced projects fetch from github by data file

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,46 +1,48 @@
 import './portfolio.css';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import portfolioData from './portfolioData';
 
 function Portfolio() {    
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch('https://api.github.com/users/mayanklad/repos')
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        throw response;
-      })
-      .then(data => {
-        console.log(data)
-        setData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data: ', error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      })
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://api.github.com/users/mayanklad/repos')
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return response.json()
+  //       }
+  //       throw response;
+  //     })
+  //     .then(data => {
+  //       console.log(data)
+  //       setData(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching data: ', error);
+  //       setError(error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     })
+  // }, []);
 
   return (
     <div id='projects' className='portfolio-container px-2.5 py-6 w-full'>
       <h1 className='px-3.5'>Projects</h1>
       <div className='content px-3.5 w-full flex flex-col items-center'>
         <div className='flex flex-row flex-wrap'>
-          {data.map((project) => {
+          {portfolioData.map((project, index) => {
             return (
               <div
-                key={project.id}
+                key={index}
                 className='group relative w-full aspect-video mb-2.5'
               >
                 <img
                   className='absolute w-full h-full m-0 -z-20' 
-                  src='https://naledi.co.uk/wp-content/uploads/2022/07/10-768x392.png'
+                  src={project.imgUrl}
+                  alt={project.name}
                 ></img>
                 <div className='absolute w-full h-full bg-[#5988ff] opacity-0 -z-10 group-hover:opacity-80 transition duration-500 ease-in'></div>
                 <div className='flex flex-col p-5 w-full h-full items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 ease-in'>
